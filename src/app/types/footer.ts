f3b8645241520f4467d5da9e18757ce00ee95b45
Lyhour Seng <lyhourlucky77@gmail.com>
2026-07-21 23:56:33 +0700

    feat: add footer

---
+++ src/app/types/footer.ts
@@ -0,0 +1,10 @@
+export type footerProps = {
+    id: number,
+    icons: React.ReactNode,
+    url: string
+}
+export type resourceProps = {
+    id: number,
+    title: string,
+    category: string[]
+}
